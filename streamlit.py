906a79bc41e208f6bfc0f4d00d3ec3c42ac85f3b
Adarsh <37295203+Adarsh0047@users.noreply.github.com>
2023-03-11 16:00:26 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -3,7 +3,7 @@ from PIL import Image
 import tensorflow as tf
 import numpy as np
 import wget
-st.set_page_config(page_title="The Weather App")
+st.set_page_config(page_title="The Weather App", layout="wide")
 st.header("Weather Classifier")
 def load_model(path):
     return tf.keras.models.load_model(path)
