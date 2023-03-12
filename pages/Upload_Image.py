6c4b46b043f3a3150f29a17022209cec7baf1681
Adarsh <37295203+Adarsh0047@users.noreply.github.com>
2023-03-12 22:49:04 +0530

    Update Upload_Image.py

--- pages/Upload_Image.py
+++ pages/Upload_Image.py
@@ -3,6 +3,11 @@ from PIL import Image
 import tensorflow as tf
 import numpy as np
 st.set_page_config(layout="wide",page_icon="upload.png")
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+local_css("style.css")
 st.header("Weather Classifier")
 @st.cache_resource
 def load_model(path):
