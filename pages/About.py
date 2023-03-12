6a83d1b0785295c9791a40afe3c5bdd6ce0cbb70
Adarsh <37295203+Adarsh0047@users.noreply.github.com>
2023-03-12 22:55:59 +0530

    Update About.py

--- pages/About.py
+++ pages/About.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.set_page_config(page_icon="about.png",layout="wide")
+st.set_page_config("About", page_icon="about.png",layout="wide")
 st.header("This app is used to predict the weather from images")
 with st.expander("Know More"):
     st.write("The images can be uploaded from a local device or as a url from the web ")
